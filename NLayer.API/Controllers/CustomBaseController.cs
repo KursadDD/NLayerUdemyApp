6f16b42985ef4cf1f9852d1ab827cc8112440009
Kursad <kursad.demirdogen@gmail.com>
2023-04-08 01:37:44 +0300

    custom base controller added

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction] 
+        //bu metodun bir endpoint olmadığını gösterir, aksi halde swagger bunu bir end point olarak algıla
+        //içeirisinde de get, post olmadığı için hata verir
+        public IActionResult CreateActionResult<T>(CustomeResponseDto<T> response)
+        {
+            if (response.StatusCode == 204)
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
