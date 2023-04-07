94da8015582e14dd56ba7f9acc30367d069b93bb
Kursad <kursad.demirdogen@gmail.com>
2023-04-08 01:37:58 +0300

    productcontroller added

---
+++ NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductsController(IService<Product> service, IMapper mapper)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CreateActionResult(CustomeResponseDto<List<ProductDto>>.Success(200, productsDtos));
+        }
+
+        //  /api/product/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            var productsDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomeResponseDto<ProductDto>.Success(200, productsDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var productsDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomeResponseDto<ProductDto>.Success(201, productsDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));
+            return CreateActionResult(CustomeResponseDto<NoContentDto>.Success(204));
+        }
+
+        //  /api/product/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            await _service.RemoveAsync(product);
+            return CreateActionResult(CustomeResponseDto<NoContentDto>.Success(204));
+        }
+    }
+}
