b05193f1f9915505997df6287cab07bf9f2655f0
Kursad <kursad.demirdogen@gmail.com>
2023-04-30 23:00:22 +0300

    categories controller added

---
+++ NLayer.Core/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using NLayer.Core.Models;
+using NLayer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface ICategoryService : IService<Category>
+    {
+        public Task<CustomeResponseDto<CategoryWithProductsDto>>GetSingleCategoryByIdWithProductsAsync(int categoryId);
+    }
+}
