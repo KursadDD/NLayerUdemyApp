9d5e500110efb5c720157253a32df3b0714a4fd8
Kursad <kursad.demirdogen@gmail.com>
2023-05-19 23:07:55 +0300

    fluent validaton added

---
+++ NLayer.API/Filters/ValidateFilterAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NLayer.API.Filters
+{
+    public class ValidateFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+    }
+}
